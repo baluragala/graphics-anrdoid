99334bf840d4ed2ea9d46254050a8acce8784c7f
Bala Krishna Ragala <baluragala@sparity.com>
2016-12-03 18:37:16 +0530

    Clipping done

--- app/src/main/java/com/moodmetric/graphics/MainActivity.java
+++ app/src/main/java/com/moodmetric/graphics/MainActivity.java
@@ -1,82 +1,58 @@
 package com.moodmetric.graphics;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.graphics.Rect;
 import android.graphics.RectF;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.ActionBar;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static android.R.attr.centerX;
-import static android.R.attr.data;
-import static android.R.attr.left;
-import static android.R.attr.path;
-import static android.R.attr.radius;
+import static android.R.attr.format;
 import static android.R.attr.x;
 import static android.R.attr.y;
-import static com.moodmetric.graphics.R.attr.maxSpeed;
-import static com.moodmetric.graphics.R.attr.thickness;
 
 public class MainActivity extends AppCompatActivity {
 
     private Paint backgroundPaint;
-    private Paint backgroundInnerPaint;
-    private Paint maskPaint;
-    private Paint needlePaint;
     private Paint ticksPaint;
-    private Paint txtPaint;
     private Paint colorLinePaint;
-    private Paint colorl0paint;
-    private Paint colorl1paint;
-    private Paint colorl2paint;
-    private Paint colorl3paint;
-    private Paint colorl4paint;
-    private Paint colorl5paint;
+    private Paint colorCautiousRedPaint;
+    private Paint colorIntensePurplePaint;
+    private Paint colorLogoTurquoisePaint;
+    private Paint colorFlowTurquoisePaint;
+    private Paint colorCalmTurquoisePaint;
+    private Paint colorGroundedBeigePaint;
+    private Paint outerCirclePaint;
 
-    private int colorLogoTurquoise=Color.parseColor("#008f67");
-    private int colorFlowTurquoise=Color.parseColor("#4bb494");
-    private int colorCalmTurquoise=Color.parseColor("#98c399");
-    private int colorGroundedBeige=Color.parseColor("#e4d599");
-    private int colorIntensePurple=Color.parseColor("#824d90");
-    private int colorCautiousRed=Color.parseColor("#f1471d");
+    private int colorLogoTurquoise = Color.parseColor("#008f67");
+    private int colorFlowTurquoise = Color.parseColor("#4bb494");
+    private int colorCalmTurquoise = Color.parseColor("#98c399");
+    private int colorGroundedBeige = Color.parseColor("#e4d599");
+    private int colorIntensePurple = Color.parseColor("#824d90");
+    private int colorCautiousRed = Color.parseColor("#f1471d");
 
-
-    private View myView;
-    private boolean toggleDayNight = false;
+    private View FlowerView;
 
     public static final double DEFAULT_MAX_SPEED = 100.0;
     public static final double DEFAULT_MAJOR_TICK_STEP = 20.0;
     public static final int DEFAULT_MINOR_TICKS = 1;
-    public static final int DEFAULT_LABEL_TEXT_SIZE_DP = 12;
-
-    private double maxSpeed = DEFAULT_MAX_SPEED;
-    private double speed = 0;
     private int defaultColor = Color.rgb(180, 180, 180);
-    private double majorTickStep = DEFAULT_MAJOR_TICK_STEP;
-    private int minorTicks = DEFAULT_MINOR_TICKS;
-
-    private List<ColoredRange> ranges = new ArrayList<ColoredRange>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        myView = new MyView(getApplicationContext());
-        setContentView(myView);
+        FlowerView = new MyView(getApplicationContext());
+        setContentView(FlowerView);
     }
 
     class MyView extends View {
@@ -93,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
             super(context, attrs, defStyleAttr);
         }
 
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         public MyView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
             super(context, attrs, defStyleAttr, defStyleRes);
         }
@@ -119,39 +96,29 @@ public class MainActivity extends AppCompatActivity {
             colorLinePaint.setStrokeWidth(5);
             colorLinePaint.setColor(defaultColor);
 
-            colorl0paint = new Paint();
-            colorl0paint.setStyle(Paint.Style.FILL);
-            colorl0paint.setColor(colorCautiousRed);
+            colorCautiousRedPaint = new Paint();
+            colorCautiousRedPaint.setStyle(Paint.Style.FILL);
+            colorCautiousRedPaint.setColor(colorCautiousRed);
 
-            colorl1paint = new Paint();
-            colorl1paint.setStyle(Paint.Style.FILL);
-            colorl1paint.setColor(colorIntensePurple);
+            colorIntensePurplePaint = new Paint();
+            colorIntensePurplePaint.setStyle(Paint.Style.FILL);
+            colorIntensePurplePaint.setColor(colorIntensePurple);
 
-            colorl2paint = new Paint();
-            colorl2paint.setStyle(Paint.Style.FILL);
-            colorl2paint.setColor(colorLogoTurquoise);
+            colorLogoTurquoisePaint = new Paint();
+            colorLogoTurquoisePaint.setStyle(Paint.Style.FILL);
+            colorLogoTurquoisePaint.setColor(colorLogoTurquoise);
 
-            colorl3paint = new Paint();
-            colorl3paint.setStyle(Paint.Style.FILL);
-            colorl3paint.setColor(colorFlowTurquoise);
+            colorFlowTurquoisePaint = new Paint();
+            colorFlowTurquoisePaint.setStyle(Paint.Style.FILL);
+            colorFlowTurquoisePaint.setColor(colorFlowTurquoise);
 
-            colorl4paint = new Paint();
-            colorl4paint.setStyle(Paint.Style.FILL);
-            colorl4paint.setColor(colorCalmTurquoise);
+            colorCalmTurquoisePaint = new Paint();
+            colorCalmTurquoisePaint.setStyle(Paint.Style.FILL);
+            colorCalmTurquoisePaint.setColor(colorCalmTurquoise);
 
-            colorl5paint = new Paint();
-            colorl5paint.setStyle(Paint.Style.FILL);
-            colorl5paint.setColor(colorGroundedBeige);
-
-            double w = getWidth();
-            double h = getHeight();
-            double ox = w/2;
-            double oy = h/2;
-            double r = Math.min(ox,oy)-20;
-            double rmax = r;
-            double rmin = 30.0;
-
-
+            colorGroundedBeigePaint = new Paint();
+            colorGroundedBeigePaint.setStyle(Paint.Style.FILL);
+            colorGroundedBeigePaint.setColor(colorGroundedBeige);
 
             ticksPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
             ticksPaint.setStrokeWidth(3.0f);
@@ -159,41 +126,27 @@ public class MainActivity extends AppCompatActivity {
             ticksPaint.setColor(colorCautiousRed);
             ticksPaint.setTextSize(60);
 
-            RectF circle = getCircle(canvas, 1);
-            canvas.drawArc(circle, 90, 360, true, colorl0paint);
+            double w = getWidth();
+            double h = getHeight();
+            double ox = w / 2;
+            double oy = h / 2;
+            double r = Math.min(ox, oy) - 20;
+            double rmax = r;
+            double rmin = 30.0;
 
-            RectF Outcircle = getCircle(canvas, 1);
-            canvas.drawArc(Outcircle, 90, 360, true, colorLinePaint);
-
-            RectF innerCircle1 = getCircle(canvas, 0.9f);
-            canvas.drawArc(innerCircle1, 90, 360, true, colorl1paint);
-
-            RectF innerCircle2 = getCircle(canvas, 0.8f);
-            canvas.drawArc(innerCircle2, 90, 360, true, colorl2paint);
-
-            RectF innerCircle3 = getCircle(canvas, 0.7f);
-            canvas.drawArc(innerCircle3, 90, 360, true, colorl3paint);
-
-            RectF innerCircle4 = getCircle(canvas, 0.6f);
-            canvas.drawArc(innerCircle4, 90, 360, true, colorl4paint);
-
-            RectF innerCircle5 = getCircle(canvas, 0.5f);
-            canvas.drawArc(innerCircle5, 90, 360, true, colorl5paint);
-
-            //fillCircle(canvas,(float)ox,(float)oy,(float)(1*rmax),colorl0paint);
-
-            Path path=new Path();
+            canvas.save();
+            Path path = new Path();
             List<MMPointer> data = simulateData();
-            Double tprev=null;
-            double t,v;
-            for(MMPointer p:data){
-                t=p.t;
-                v=p.v;
-                double a= ( 3 * Math.PI/2) - t * 2* Math.PI / (12*3600);
-                double rv = rmin + v/100*(rmax-rmin);
-                double xv = ox + (float)(rv * Math.cos(a));
-                double yv = oy - (float)(rv * Math.sin(a));
-                if(tprev == null) {
+            Double tprev = null;
+            double t, v;
+            for (MMPointer p : data) {
+                t = p.t;
+                v = p.v;
+                double a = (3 * Math.PI / 2) - t * 2 * Math.PI / (12 * 3600);
+                double rv = rmin + v / 100 * (rmax - rmin);
+                double xv = ox + (float) (rv * Math.cos(a));
+                double yv = oy - (float) (rv * Math.sin(a));
+                if (tprev == null) {
                     path.moveTo((float) ox, (float) oy);
                 } else if (t - tprev.doubleValue() > 120) {
                     path.lineTo((float) ox, (float) oy);
@@ -202,9 +155,79 @@ public class MainActivity extends AppCompatActivity {
                 tprev = t;
             }
             path.close();
-            canvas.drawPath(path,backgroundPaint);
+            canvas.drawPath(path, backgroundPaint);
+            canvas.clipPath(path);
+
+            RectF circle = getCircle(canvas, 1);
+            canvas.drawArc(circle, 90, 360, true, colorCautiousRedPaint);
+
+
+            RectF Outcircle = getCircle(canvas, 1);
+            canvas.drawArc(Outcircle, 90, 360, true, colorLinePaint);
+
+            RectF innerCircle1 = getCircle(canvas, 0.9f);
+            canvas.drawArc(innerCircle1, 90, 360, true, colorIntensePurplePaint);
+
+            RectF innerCircle2 = getCircle(canvas, 0.8f);
+            canvas.drawArc(innerCircle2, 90, 360, true, colorLogoTurquoisePaint);
+
+            RectF innerCircle3 = getCircle(canvas, 0.7f);
+            canvas.drawArc(innerCircle3, 90, 360, true, colorFlowTurquoisePaint);
+
+            RectF innerCircle4 = getCircle(canvas, 0.6f);
+            canvas.drawArc(innerCircle4, 90, 360, true, colorCalmTurquoisePaint);
+
+            RectF innerCircle5 = getCircle(canvas, 0.5f);
+            canvas.drawArc(innerCircle5, 90, 360, true, colorGroundedBeigePaint);
+
+            canvas.restore();
+
+            //draw centre button place
+            canvas.save();
+            Paint centerCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            centerCirclePaint.setShadowLayer(100, 0, 0, Color.BLACK);
+            centerCirclePaint.setColor(Color.WHITE);
+            RectF innerCircle = getCircle(canvas, 0.18f);
+            canvas.drawArc(innerCircle, 90, 360, true, centerCirclePaint);
+            canvas.restore();
+
+            //draw clock table
+            outerCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            outerCirclePaint.setStyle(Paint.Style.STROKE);
+            outerCirclePaint.setStrokeWidth(5);
+            outerCirclePaint.setColor(colorFlowTurquoise);
+            outerCirclePaint.setTextSize(60);
+            Path outerCirclePath = new Path();
+            RectF outerCircle = getCircle(canvas, 1);
+            outerCirclePath.addArc(outerCircle, 90, 360);
+            outerCirclePath.moveTo((float) ox, (float) (oy + rmin));
+            outerCirclePath.lineTo((float) ox, (float) (oy + r));
+            canvas.drawPath(outerCirclePath, outerCirclePaint);
 
             //drawTicks(canvas);
+
+            // Draw ticks
+
+            int tickr = (int)(r+10);
+            for(int i=0;i<=3;i++) {
+                double angle = (i+1)*Math.PI/2;
+                String tick = String.format("%d", 12-i*3);
+                double tx = ox + tickr*(float)(Math.cos(angle))-40;// - sz.width/2
+                double ty = oy - tickr*(float)(Math.sin(angle))-40;// - sz.height/2
+                canvas.drawText(tick,(float) tx,(float) ty,outerCirclePaint);
+            }
+            Path tickPath=new Path();
+            for(int i=0;i<=11;i++) {
+                double angle = i*Math.PI/6;
+                double x1 = ox + (r-5)*(float)(Math.cos(angle));
+                double y1 = oy - (r-5)*(float)(Math.sin(angle));
+                double x2 = ox + r*(float)(Math.cos(angle));
+                double y2 = oy - r*(float)(Math.sin(angle));
+                tickPath.moveTo((float) x1, (float) y1);
+                tickPath.lineTo((float) x2, (float) y2);
+                canvas.drawPath(tickPath,outerCirclePaint);
+            }
+
 
             /*
 
@@ -216,16 +239,6 @@ public class MainActivity extends AppCompatActivity {
             */
 
         }
-
-
-        private void fillCircle(Canvas canvas,float x, float y, float radius, Paint paint) {
-            Path p=new Path();
-            p.addArc(new RectF(0, 0, getWidth(),getWidth()), 0.0f, 360.0f);
-            p.close();
-            canvas.drawPath(p,paint);
-            //canvas.save();
-        }
-
 
         private RectF getCircle(Canvas canvas, float factor) {
             RectF oval;
@@ -245,7 +258,7 @@ public class MainActivity extends AppCompatActivity {
         private void drawTicks(Canvas canvas) {
             float majorStep = 30;
             float majorTicksLength = 30;
-            RectF oval = getCircle(canvas, 0.85f);
+            RectF oval = getCircle(canvas, 1f);
             float radius = oval.width() * 0.5f;
             float currentAngle = 90;
             int tickCount = 0;
@@ -260,7 +273,7 @@ public class MainActivity extends AppCompatActivity {
                 if (tickCount == 12 || tickCount == 3 || tickCount == 6 || tickCount == 9)
                     canvas.drawText(String.valueOf(tickCount == 0 ? 12 : tickCount),
                             (float) (oval.centerX() + Math.cos((180 - currentAngle) / 180 * Math.PI) * (radius - majorTicksLength / 2)),
-                            (float) (oval.centerY() - Math.sin(currentAngle / 180 * Math.PI) * (radius + majorTicksLength / 2)), ticksPaint);
+                            (float) (oval.centerY() - Math.sin(currentAngle / 180 * Math.PI) * (radius + majorTicksLength / 2)), outerCirclePaint);
                 tickCount++;
                 currentAngle += majorStep;
 
@@ -273,7 +286,7 @@ public class MainActivity extends AppCompatActivity {
 
             if (event.getAction() == MotionEvent.ACTION_UP && event.getX() > getWidth() / 2 - 40 && event.getX() < getWidth() / 2 + 40 && event.getY() > getHeight() / 2 - 40 && event.getY() < getHeight() / 2 + 40)
                 //Log.d("onTouchEvent",event.getX()+"," + event.getY());
-                myView.invalidate();
+                FlowerView.invalidate();
             return true;
         }
     }
